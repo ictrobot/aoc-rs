15dcd8aa642bfbbfdf99e0fcd82a7484b4360fd6
ictrobot <ictrobot@outlook.com>
2024-08-11 17:16:55 +0100

    Speed up 2015 day 20
    Reuse work between parts 1 & 2 for ~2x speedup.
    
    Try smaller upper bounds first for a further ~3x speedup.

--- crates/year2015/src/day20.rs
+++ crates/year2015/src/day20.rs
@@ -3,45 +3,78 @@ use utils::prelude::*;
 /// Divisor function.
 #[derive(Clone, Debug)]
 pub struct Day20 {
-    threshold: u32,
+    part1: u32,
+    part2: u32,
 }
 
 impl Day20 {
     pub fn new(input: &str, _: InputType) -> Result<Self, InputError> {
-        Ok(Self {
-            threshold: input
-                .trim_ascii_end()
-                .parse()
-                .map_err(|_| InputError::new(input, 0, "expected u32"))?,
-        })
-    }
+        let threshold: u32 = input
+            .trim_ascii_end()
+            .parse()
+            .map_err(|_| InputError::new(input, 0, "expected u32"))?;
 
-    #[must_use]
-    pub fn part1(&self) -> usize {
-        let threshold = self.threshold / 10;
-        let mut buckets = vec![0; threshold as usize];
-
-        for elf in 1..threshold {
-            for i in (elf..threshold).step_by(elf as usize) {
-                buckets[i as usize] += elf;
+        // (threshold/10) + 1 should always find the solutions, but attempt to find the solutions
+        // with smaller & significantly faster upper bounds first
+        for max_bound in [
+            threshold.div_ceil(40) + 1,
+            threshold.div_ceil(20) + 1,
+            threshold.div_ceil(10) + 1,
+        ] {
+            if let Some((part1, part2)) = Self::try_solve(threshold, max_bound) {
+                return Ok(Day20 { part1, part2 });
             }
         }
 
-        buckets.iter().position(|&x| x >= threshold).unwrap_or(0)
+        unreachable!();
     }
 
-    #[must_use]
-    pub fn part2(&self) -> usize {
-        let threshold = self.threshold / 11;
-        let mut buckets = vec![0u32; threshold as usize];
+    fn try_solve(threshold: u32, len: u32) -> Option<(u32, u32)> {
+        let mut houses = vec![0; len as usize];
 
-        for elf in 1..threshold {
-            for i in (elf..threshold).step_by(elf as usize).take(50) {
-                buckets[i as usize] += elf;
+        // Elves < len/50 would visit more than 50 houses, visit the first 50 for part 2 solution
+        for elf in 1..len / 50 {
+            for house in (elf..len).step_by(elf as usize).take(50) {
+                houses[house as usize] += elf;
             }
         }
 
-        buckets.iter().position(|&x| x >= threshold).unwrap_or(0)
+        // Elves > len/50 will visit at most 50 houses
+        for elf in (len / 50).max(1)..len / 2 {
+            for house in (elf..len).step_by(elf as usize) {
+                houses[house as usize] += elf;
+            }
+        }
+
+        // Elves >= len/2 will visit 1 house
+        for elf in (len / 2).max(1)..len {
+            houses[elf as usize] += elf;
+        }
+
+        let part2_threshold = threshold.div_ceil(11);
+        let part2 = houses.iter().position(|&c| c >= part2_threshold)?;
+
+        // After finding part 2 solution, process the previously skipped houses for elves < len/50
+        for elf in 1..len / 50 {
+            for house in (elf * 51..len).step_by(elf as usize) {
+                houses[house as usize] += elf;
+            }
+        }
+
+        let part1_threshold = threshold.div_ceil(10);
+        let part1 = houses.iter().position(|&c| c >= part1_threshold)?;
+
+        Some((part1 as u32, part2 as u32))
+    }
+
+    #[must_use]
+    pub fn part1(&self) -> u32 {
+        self.part1
+    }
+
+    #[must_use]
+    pub fn part2(&self) -> u32 {
+        self.part2
     }
 }
 
