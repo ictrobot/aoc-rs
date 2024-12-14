88e9596989fefd6152cfc9233bf0255b3c309056
ictrobot <ictrobot@outlook.com>
2024-12-14 06:33:58 +0000

    Optimize 2024 day 14
    Use u128 for each row, reducing runtime from ~60ms to ~24ms

--- crates/year2024/src/day14.rs
+++ crates/year2024/src/day14.rs
@@ -16,6 +16,7 @@ struct Robot {
     velocity: Point2D<i32>,
 }
 
+// WIDTH must be less than 128
 const WIDTH: i32 = 101;
 const HEIGHT: i32 = 103;
 
@@ -63,27 +64,32 @@ impl Day14 {
     pub fn part2(&self) -> u32 {
         let mut robots = self.robots.clone();
         for i in 1..=u32::MAX {
-            let mut grid = [false; (WIDTH + 1) as usize * HEIGHT as usize];
+            let mut grid = [0u128; HEIGHT as usize];
             for r in robots.iter_mut() {
                 r.position += r.velocity;
                 r.position.x = r.position.x.rem_euclid(WIDTH);
                 r.position.y = r.position.y.rem_euclid(HEIGHT);
-                grid[r.position.y as usize * (WIDTH + 1) as usize + r.position.x as usize] = true;
+                grid[r.position.y as usize] |= 1 << r.position.x;
             }
 
-            let mut count = 0;
-            for b in grid {
-                if b {
-                    count += 1;
-                    if count >= 10 {
-                        return i;
-                    }
-                } else {
-                    count = 0;
-                }
+            if grid.iter().any(|&b| Self::has_ten_consecutive_bits(b)) {
+                return i;
             }
         }
         unreachable!("no solution found")
+    }
+
+    fn has_ten_consecutive_bits(b: u128) -> bool {
+        b & (b << 1)
+            & (b << 2)
+            & (b << 3)
+            & (b << 4)
+            & (b << 5)
+            & (b << 6)
+            & (b << 7)
+            & (b << 8)
+            & (b << 9)
+            != 0
     }
 }
 
