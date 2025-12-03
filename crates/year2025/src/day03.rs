9614b382d7e817677cafb0dc42ee8520057582db
ictrobot <ictrobot@outlook.com>
2025-12-03 15:51:15 +0000

    Optimize 2025 day 3

--- crates/year2025/src/day03.rs
+++ crates/year2025/src/day03.rs
@@ -3,18 +3,18 @@ use utils::prelude::*;
 /// Finding the maximum number from ordered digits.
 #[derive(Clone, Debug)]
 pub struct Day03<'a> {
-    lines: Vec<&'a str>,
+    lines: Vec<&'a [u8]>,
 }
 
 impl<'a> Day03<'a> {
     pub fn new(input: &'a str, _: InputType) -> Result<Self, InputError> {
-        let lines: Vec<&str> = input.lines().collect();
-        for &l in &lines {
-            if let Some(b) = l.bytes().find(|x| !x.is_ascii_digit()) {
-                return Err(InputError::new(input, b as char, "expected digit"));
+        let lines: Vec<&[u8]> = input.lines().map(str::as_bytes).collect();
+        for &line in &lines {
+            if let Some(&b) = line.iter().find(|x| !matches!(x, b'1'..=b'9')) {
+                return Err(InputError::new(input, b as char, "expected digit 1-9"));
             }
-            if l.len() < 12 {
-                return Err(InputError::new(input, l, "expected at least 12 digits"));
+            if line.len() < 12 {
+                return Err(InputError::new(input, line, "expected at least 12 digits"));
             }
         }
         Ok(Self { lines })
@@ -32,33 +32,20 @@ impl<'a> Day03<'a> {
 
     fn output_joltage<const N: usize>(&self) -> u64 {
         let mut sum = 0;
-        for l in &self.lines {
-            let mut nums = [0u8; N];
-            let (first, last) = l.as_bytes().split_last_chunk::<N>().unwrap();
-
-            for &b in first {
-                for j in 0..N {
-                    let x = b - b'0';
-                    if x > nums[j] {
-                        nums[j] = x;
-                        nums[j + 1..].fill(0);
-                        break;
-                    }
-                }
+        for line in &self.lines {
+            let (mut start, mut joltage) = (0, 0);
+            for i in 0..N {
+                let (max_byte, start_offset) =
+                    line[start..line.len() - N + 1 + i].iter().enumerate().fold(
+                        (0, 0),
+                        |(max, offset), (i, &b)| {
+                            if b > max { (b, i) } else { (max, offset) }
+                        },
+                    );
+                joltage = (joltage * 10) + (max_byte - b'0') as u64;
+                start += start_offset + 1;
             }
-
-            for (i, &b) in last.iter().enumerate() {
-                for j in i..N {
-                    let x = b - b'0';
-                    if x > nums[j] {
-                        nums[j] = x;
-                        nums[j + 1..].fill(0);
-                        break;
-                    }
-                }
-            }
-
-            sum += nums.iter().fold(0, |acc, &x| acc * 10 + x as u64);
+            sum += joltage;
         }
         sum
     }
